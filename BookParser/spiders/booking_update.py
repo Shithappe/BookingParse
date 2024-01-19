93db1da42274a6a5fb5ec786a3a776e8b4c8bff8
Shithappe <greenmad.b50@gmail.com>
2024-01-20 01:37:00 +0200

    fix cite and type

--- BookParser/spiders/booking_update.py
+++ BookParser/spiders/booking_update.py
@@ -77,7 +77,7 @@ class MySpider(scrapy.Spider):
         facilities_data = self.cursor.fetchall()
         self.facilities_cache = {title: id for id, title in facilities_data}
 
-        self.cursor.execute('SELECT id, link FROM booking_data LIMIT 1100, 11043')
+        self.cursor.execute('SELECT id, link FROM booking_data')
         booking_data = self.cursor.fetchall()
 
         for book in booking_data:
@@ -92,43 +92,57 @@ class MySpider(scrapy.Spider):
         booking_id = response.meta.get('id')
 
         title = response.css('.d2fee87262.pp-header__title::text').extract_first()
-        description = response.css('.a53cbfa6de.b3efd73f69::text').extract_first().strip()
+        # description = response.css('.a53cbfa6de.b3efd73f69::text').extract_first().strip()
 
-        star = len(response.css('span[data-testid="rating-stars"] > span'))
-        address = response.css('span.hp_address_subtitle::text').get().strip()
-        if len(address.split(',')) > 3:
-            city = re.sub(r'\d+', '', address.split(',')[3]).strip()
-        else:
-            city = re.sub(r'\d+', '', address.split(',')[1]).strip()
+        # star = len(response.css('span[data-testid="rating-stars"] > span'))
+        # address = response.css('span.hp_address_subtitle::text').get().strip()
+        city = response.css('input[name="ss"]::attr(value)').extract_first().strip()
 
-        location = response.css('a#hotel_address').attrib.get('data-atlas-latlng')
+        # location = response.css('a#hotel_address').attrib.get('data-atlas-latlng')
 
-        score = response.css('div.a3b8729ab1.d86cee9b25::text').get()
+        # score = response.css('div.a3b8729ab1.d86cee9b25::text').get()
 
-        images = response.css('a.bh-photo-grid-item.bh-photo-grid-thumb > img::attr(src)').extract()
+        # images = response.css('a.bh-photo-grid-item.bh-photo-grid-thumb > img::attr(src)').extract()
 
 
-        link = response.meta.get('original_url').split('?')[0]
+        # link = response.meta.get('original_url').split('?')[0]
 
-        type = response.css('li[itemprop="itemListElement"][data-google-track*="hotel"] a::text')[1].get()
-        if 'All' in type:
-            type = type.replace('All', '').strip().capitalize()
-        else:
-            type = 'Guest houses'
+        type = None
+        avalible_types = ["Villa", "Villas", "Guesthouse", "Homestay", "Bungalows", "Resort"]
+        
+        for word in avalible_types:
+            if word in title:
+                type = word
 
-        review_count = int(response.xpath('//*[@id="js--hp-gallery-scorecard"]/a/div/div/div/div[2]/div[2]/text()').get().split()[-2].replace(',', ''))
+        if type is None:
+            type = response.css('.bui-breadcrumb__text a.bui_breadcrumb__link::text').getall()[2]
+            type_match = re.search(r'All\s+(.+)', type)
+            type = (type_match.group(1) if type_match else 'Hotel').capitalize()
+            
 
+        # review_count = int(response.xpath('//*[@id="js--hp-gallery-scorecard"]/a/div/div/div/div[2]/div[2]/text()').get().split()[-2].replace(',', ''))
+
+        # print(f'\n{title} --- {type}\n')
         
         sql = """
             UPDATE booking_data 
-            SET title = %s, description = %s, star = %s, link = %s, address = %s, city = %s,
-            location = %s, score = %s, images = %s, type = %s, review_count = %s
+            SET city = %s, type = %s
             WHERE id = %s
         """
         self.cursor.execute(sql, (
-            title, description, star, link, address, city, location, score, str(images), type, review_count, booking_id
+            city, type, booking_id
         ))
         self.connection.commit()
+        # sql = """
+        #     UPDATE booking_data 
+        #     SET title = %s, description = %s, star = %s, link = %s, address = %s, city = %s,
+        #     location = %s, score = %s, images = %s, type = %s, review_count = %s
+        #     WHERE id = %s
+        # """
+        # self.cursor.execute(sql, (
+        #     title, description, star, link, address, city, location, score, str(images), type, review_count, booking_id
+        # ))
+        # self.connection.commit()
 
 
 
