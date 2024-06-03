8b8996e1a28ef0b18523b279074de9c3920754fa
Shithappe <greenmad.b50@gmail.com>
2024-06-03 20:00:12 +0300

    price for rooms_2_day

--- BookParser/spiders/rooms_2_day.py
+++ BookParser/spiders/rooms_2_day.py
@@ -12,7 +12,7 @@ class UpdateRoomsSpider(scrapy.Spider):
     
 
     today = datetime.now().date()
-    today = today + timedelta(days=2)  # for debug
+    # today = today + timedelta(days=2)  # for debug
 
     checkin = today + timedelta(hours=8)
     checkout = checkin + timedelta(days=1)    
@@ -25,14 +25,6 @@ class UpdateRoomsSpider(scrapy.Spider):
     
 
     def connect_to_db(self):
-         
-        config_local = {
-            'user': 'root',
-            'password': '1234',
-            'host': 'localhost',
-            'database': 'parser_booking',
-            'raise_on_warnings': True
-        }
 
         config = {
             'user': 'artnmo_estate',
@@ -66,18 +58,19 @@ class UpdateRoomsSpider(scrapy.Spider):
         return urlunparse(url)
 
     def set_to_db(self, booking_id, value, checkin, checkout):
+        print('\nWRITE TO DB\n')
         print(booking_id, value, checkin, checkout)
-        # try:
-        #     self.cursor.executemany("""
-        #         INSERT INTO rooms_2_day
-        #         (booking_id, room_type, available_rooms, checkin, checkout)
-        #         VALUES (%s, %s, %s, %s, %s)
-        #     """, [(booking_id, room_type, count, checkin, checkout) for room_type, count in value])
+        try:
+            self.cursor.executemany("""
+                INSERT INTO rooms_2_day
+                (booking_id, room_type, available_rooms, price, checkin, checkout)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """, [(booking_id, room_type, count, price, checkin, checkout) for room_type, count, price in value])
 
-        #     self.connection.commit()
-        #     # print("Insert successful")
-        # except Exception as e:
-        #     print(f"Error: {e}")
+            self.connection.commit()
+            # print("Insert successful")
+        except Exception as e:
+            print(f"Error: {e}")
 
         
 
@@ -112,8 +105,8 @@ class UpdateRoomsSpider(scrapy.Spider):
             rows = self.cursor.fetchall()
 
         
-        self.cursor.execute(f'SELECT id, link FROM booking_data WHERE id = 1')
-        rows = self.cursor.fetchall()
+        # self.cursor.execute(f'SELECT id, link FROM booking_data WHERE id = 2017')
+        # rows = self.cursor.fetchall()
 
         for row in rows:
                 formatted_link = self.format_link(row[1], self.checkin, self.checkout) 
@@ -129,31 +122,32 @@ class UpdateRoomsSpider(scrapy.Spider):
 
         booking_id = response.meta.get('booking_id')
 
-        self.cursor.execute(f'''SELECT room_type, MAX(max_available_rooms) AS max_available
-                            FROM rooms_30_day
-                            WHERE booking_id = {booking_id}
+        self.cursor.execute(f'''SELECT room_type, max_available
+                            FROM rooms
+                            WHERE booking_id = {booking_id} and active = 1
                             GROUP BY room_type''')
         max_available = self.cursor.fetchall()
 
         checkin = response.meta.get('checkin')
         checkout = response.meta.get('checkout')        
-        
+
         room_type = None
         available_rooms = None
         rowspan = None
-        
+
         rows = response.xpath('//*[@id="hprt-table"]/tbody/tr')
 
         room_types_count = {}
+        room_prices = {}
 
-        if (rows):
+        if rows:
             for i in range(len(rows)):
                 rowspan = rows[i].xpath('./td/@rowspan').get()
                 if rowspan:
                     room_type = rows[i].xpath('.//span[contains(@class, "hprt-roomtype-icon-link")]/text()').get().strip()
                     available_rooms = rows[i].xpath('.//select[@class="hprt-nos-select js-hprt-nos-select"]//option[last()]/@value').get()                
                     price = response.xpath(f'//*[@id="hprt-table"]/tbody/tr[{i+1}]/td[3]/div/div/div[1]/div[2]/div/span/text()').get()
-                    if (price):
+                    if price:
                         price = ''.join(filter(str.isdigit, price))
                     
                     print(f'{room_type}: {available_rooms} {price}')
@@ -168,6 +162,8 @@ class UpdateRoomsSpider(scrapy.Spider):
                     else:
                         room_types_count[room_type] = count
 
+                    if room_type not in room_prices:
+                        room_prices[room_type] = price
 
                     for max_type, max_count in max_available:
                         if max_type == room_type:
@@ -177,11 +173,18 @@ class UpdateRoomsSpider(scrapy.Spider):
             # Устанавливаем значение 0 для элементов, которые остались в max_available
             max_available = [(max_type, 0) for max_type, max_count in max_available]
 
-            # Объединяем room_types_count и max_available
-            combined_rooms = list(room_types_count.items()) + max_available
-            print(combined_rooms)
+            # Объединяем room_types_count, max_available и добавляем цены
+            combined_rooms = []
+            for room_type, count in room_types_count.items():
+                combined_rooms.append((room_type, count, room_prices.get(room_type)))
+
+            for max_type, _ in max_available:
+                combined_rooms.append((max_type, 0, room_prices.get(max_type)))
+
+            # print(combined_rooms)
 
             self.set_to_db(booking_id, combined_rooms, checkin, checkout)
+
 
         else:
             alert_title = response.css('.bui-alert__title::text').get()
@@ -211,8 +214,9 @@ class UpdateRoomsSpider(scrapy.Spider):
 
             else:
                 print('set 0 avalible')
-                updated_list = [(name, 0) for name, _ in max_available]
-                # print(updated_list)
+                print('max_available: ', max_available)
+                updated_list = [(name, 0, None) for name, _ in max_available]
+                print('updated_list: ', updated_list)
 
                 self.set_to_db(booking_id, updated_list, checkin, checkout)
 
